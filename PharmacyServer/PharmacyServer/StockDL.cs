ffe020fb2d735bc9bec0a5a7573ac62133aaab39
NumanArshad <37812712+NumanArshad@users.noreply.github.com>
2018-04-19 10:23:27 +0500

    I Add function delete in server

--- PharmacyServer/PharmacyServer/StockDL.cs
+++ PharmacyServer/PharmacyServer/StockDL.cs
@@ -12,35 +12,8 @@ namespace PharmacyServer
         {
             stocklist.Add(product);
         }
-        public  static List<StockClass> searchByFormula(string itemFormula)
-        {
-            List<StockClass> sel = new List<StockClass>();
-            StockClass pr = new StockClass();
-            pr.ItemFormula = itemFormula;
-            foreach(StockClass st in StockDL.stocklist)
-            {
-                if (st.ItemFormula == pr.ItemFormula)
-                {
-                    sel.Add(st);
-                }
-            }
-            return sel;
-        }
-        public static List<StockClass> searchByName(string itemName)
-        {
-            List<StockClass> sel = new List<StockClass>();
-            StockClass pr = new StockClass();
-            pr.ItemName = itemName;
-            foreach (StockClass st in StockDL.stocklist)
-            {
-                if (st.ItemName == pr.ItemName)
-                {
-                    sel.Add(st);
-                }
-            }
-            return sel;
-        }
-        //public static List<StockClass> deleteProduct(string itemName,string itemType)
+       
+
         public static List<StockClass> deleteProduct(StockClass pr)
         {
          //   List<StockClass> sel = new List<StockClass>();

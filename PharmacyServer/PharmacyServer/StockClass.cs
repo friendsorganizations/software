49900666b41c83ebd0b1efba1c44223e7d7de652
NumanArshad <37812712+NumanArshad@users.noreply.github.com>
2018-04-19 10:21:03 +0500

    i created stock clas with delete function in server

---
+++ PharmacyServer/PharmacyServer/StockClass.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmacyServer
+{
+    public class StockClass
+    {
+        private string itemName;
+        private string itemFormula;
+        private string itemType;
+        private int  itemQuantity;
+        private int itemPrice;
+        private DateTime expiryDate;
+
+        public string ItemName
+        {
+            get
+            {
+                return itemName;
+            }
+
+            set
+            {
+                itemName = value;
+            }
+        }
+
+        public string ItemFormula
+        {
+            get
+            {
+                return itemFormula;
+            }
+
+            set
+            {
+                itemFormula = value;
+            }
+        }
+
+        public string ItemType
+        {
+            get
+            {
+                return itemType;
+            }
+
+            set
+            {
+                itemType = value;
+            }
+        }
+
+        public int ItemQuantity
+        {
+            get
+            {
+                return itemQuantity;
+            }
+
+            set
+            {
+                itemQuantity = value;
+            }
+        }
+
+        public int ItemPrice
+        {
+            get
+            {
+                return itemPrice;
+            }
+
+            set
+            {
+                itemPrice = value;
+            }
+        }
+
+        public DateTime ExpiryDate
+        {
+            get
+            {
+                return expiryDate;
+            }
+
+            set
+            {
+                expiryDate = value;
+            }
+        }
+    }
+}
